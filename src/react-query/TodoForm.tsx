a6c0f0c0f2072c9b19d6ee31a8a60f0c739ea463
Izaan Khalid <izaan.khalid@outlook.com>
2024-06-28 18:53:00 -0500

    TodoForm Works

--- src/react-query/TodoForm.tsx
+++ src/react-query/TodoForm.tsx
@@ -1,18 +1,67 @@
-import { useRef } from 'react';
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import { useRef } from "react";
+import { Todo } from "./hooks/useTodos";
+import axios from "axios";
 
 const TodoForm = () => {
-  const ref = useRef<HTMLInputElement>(null);
+	const queryClient = useQueryClient();
+	const addTodo = useMutation<Todo, Error, Todo>({
+		mutationFn: (todo: Todo) =>
+			axios
+				.post<Todo>("https://jsonplaceholder.typicode.com/todos", todo)
+				.then((res) => res.data),
+		onSuccess: (savedTodo, newTodo) => {
+			// APPROACH: Invalidate the cache
+			// queryClient.invalidateQueries({
+			// 	queryKey: ["todos"],
+			// });
 
-  return (
-    <form className="row mb-3">
-      <div className="col">
-        <input ref={ref} type="text" className="form-control" />
-      </div>
-      <div className="col">
-        <button className="btn btn-primary">Add</button>
-      </div>
-    </form>
-  );
+			// APPROACH 2: Updating the data in the cache
+			queryClient.setQueryData<Todo[]>(["todos"], (todos) => [
+				savedTodo,
+				...(todos || []),
+			]);
+		},
+	});
+	const ref = useRef<HTMLInputElement>(null);
+
+	return (
+		<>
+			{addTodo.error && (
+				<div className="alert alert-danger">
+					{addTodo.error.message}
+				</div>
+			)}
+			<form
+				className="row mb-3"
+				onSubmit={(event) => {
+					event.preventDefault();
+
+					if (ref.current && ref.current.value)
+						addTodo.mutate({
+							id: 0,
+							title: ref.current?.value,
+							completed: false,
+							userId: 1,
+						});
+
+					if (ref.current) ref.current.value = "";
+				}}
+			>
+				<div className="col">
+					<input ref={ref} type="text" className="form-control" />
+				</div>
+				<div className="col">
+					<button
+						disabled={addTodo.isLoading}
+						className="btn btn-primary"
+					>
+						{addTodo.isLoading ? "Adding" : "Add"}
+					</button>
+				</div>
+			</form>
+		</>
+	);
 };
 
 export default TodoForm;
